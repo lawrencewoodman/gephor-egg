72715ac61f898ce141aaf00e69754e7d1002c91f
Lawrence Woodman <238718+lawrencewoodman@users.noreply.github.com>
2024-09-03 07:25:52 +0100

    s/boolean/false/ in router-match return type def

--- router.scm
+++ router.scm
@@ -44,7 +44,7 @@
 
 
 ;; TODO: should this return the pattern as well, perhaps using values?
-(: router-match (router string --> (or handler boolean)))
+(: router-match (router string --> (or handler false)))
 (define (router-match router selector)
   (let loop ((routes router))
     (and (not (null? routes))
